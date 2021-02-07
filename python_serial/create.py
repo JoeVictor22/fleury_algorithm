78c66695175e56f8ad4a63b27b264bee7ec75030
Joel <JoelVIctor1746@gmail.com>
2021-02-07 19:18:27 -0300

    Create create.py

---
+++ python_serial/create.py
@@ -0,0 +1,20 @@
+from pprint import pprint
+
+def create(size):
+    grafo = [[0 for i in range(size)] for j in range(size)] 
+    for i in range(size):
+        for j in range(size):
+            if j != i:
+                grafo[i][j] = 1
+    return grafo
+
+
+
+tamanho = input("Qual o tamanho da matriz a ser criada?")
+tamanho = int(tamanho)
+grafo = create(tamanho)
+
+for i in range(tamanho):
+    for j in range(tamanho):
+        print(str(grafo[i][j]) + " ", end='')
+    print("")
